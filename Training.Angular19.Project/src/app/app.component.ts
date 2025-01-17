bf38b6d36ae582e27f47f14de51f7e8a91078695
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-18 01:55:01 +0530

    Created component and rendered

--- Training.Angular19.Project/src/app/app.component.ts
+++ Training.Angular19.Project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent,AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
