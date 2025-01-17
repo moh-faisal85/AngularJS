a034ab4f87962a7f9a41a51e254cf6330168d1f6
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-18 02:20:37 +0530

    Add css without external css file

--- Training.Angular19.Project/src/app/components/user/user.component.ts
+++ Training.Angular19.Project/src/app/components/user/user.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   imports: [],
-  template: '<h1>User Page without external file </h1>',
-  
+  template: "<h1 class='text-danger'>User Page without external file </h1>",
+  styles:['.text-danger {color:blue}']
 })
 export class UserComponent {
 
