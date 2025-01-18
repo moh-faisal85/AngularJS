b8b279aa1856587fdf32a93147f758af64695ed2
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-19 01:18:23 +0530

    Part-4: Angular 19-Stuctural Directive - ngIf - Simple - Show / Hide Div based on TWO Values

--- Training.Angular19.Project/src/app/components/ng-if/ng-if.component.ts
+++ Training.Angular19.Project/src/app/components/ng-if/ng-if.component.ts
@@ -1,17 +1,19 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ng-if',
-  imports: [NgIf],
+  imports: [NgIf, FormsModule],
   templateUrl: './ng-if.component.html',
   styleUrl: './ng-if.component.css'
 })
 export class NgIfComponent {
+
 div1Visiable : boolean = true;
-/**
- *
- */
+number1:string = ''; 
+number2:string = '';
+
 constructor() {
   //alert(this.div1Visiable);
   
