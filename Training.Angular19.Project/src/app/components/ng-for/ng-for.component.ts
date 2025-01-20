de60cfafe799c4354d9b4722f26373198f9a48b5
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-21 00:23:44 +0530

    Part-4: Angular 19 - Structural Directives - ngFor - Display Dropdown list with id,name and City using employee object list

--- Training.Angular19.Project/src/app/components/ng-for/ng-for.component.ts
+++ Training.Angular19.Project/src/app/components/ng-for/ng-for.component.ts
@@ -10,4 +10,12 @@ import { Component } from '@angular/core';
 export class NgForComponent {
 
   cityList:string[] = ["Pune", "Chennai", "Mumbai", "Tirunelveli"];
+
+  employeeArray : any = [
+    {empId:1,name:'Faisal',city :'Hyderabad',contactNo:'9999999999'},
+    {empId:2,name:'Karthik',city :'Bengalure',contactNo:'8888888888'},
+    {empId:3,name:'Sambath',city :'Chennai',contactNo:'7777777777'},
+    {empId:4,name:'Mano',city :'Pune',contactNo:'6666666666'},
+    {empId:5,name:'Rajesh',city :'Mumbau',contactNo:'5555555555'}
+  ]
 }
