48d3a36a9b02420685d638d366705d9662d40366
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-21 00:59:59 +0530

    Part-5: Angular 19 - Dynamic style class implementation - 1.for div, 2.On checkbox click event, 3. Button event change class

--- Training.Angular19.Project/src/app/app.component.ts
+++ Training.Angular19.Project/src/app/app.component.ts
@@ -5,10 +5,11 @@ import { AdminComponent } from './components/admin/admin.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { NgIfComponent } from './components/ng-if/ng-if.component';
 import { NgForComponent } from './components/ng-for/ng-for.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,UserComponent,AdminComponent,DataBindingComponent,NgIfComponent,NgForComponent],
+  imports: [RouterOutlet,UserComponent,AdminComponent,DataBindingComponent,NgIfComponent,NgForComponent, NgClassComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
