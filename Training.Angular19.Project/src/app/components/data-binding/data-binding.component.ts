01d4094e7c1d6f7aa07a60695e1a550ac9a806c3
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-28 02:12:17 +0530

    Part-9-Angular19-Navigate page by clicking button event and routerLink

--- Training.Angular19.Project/src/app/components/data-binding/data-binding.component.ts
+++ Training.Angular19.Project/src/app/components/data-binding/data-binding.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { routes } from '../../app.routes';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-data-binding',
@@ -16,7 +18,7 @@ myPlaceholder:string = "Enter the Full Name";
 div1ClassName:string = "bg-primary";
 selectedCity:string  = "selectedCity";
 
-constructor() {
+constructor(private router:Router) {
   console.log(this.firstName); 
   console.log(this.rollNumber); 
   console.log(this.isActive); 
@@ -37,5 +39,9 @@ OnCityChanges(event: Event)
 OnCityChange()
 {
   alert("Selected City - Two Way Binding : "+this.selectedCity)
+}
+navigateToAdmin()
+{
+  this.router.navigateByUrl("/admin");
 }
 }
