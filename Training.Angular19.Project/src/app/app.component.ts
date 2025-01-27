e28edc37d1e6b3aed37266dedd5f1e3da375db48
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-28 01:49:03 +0530

    Part-9:Angular19-Routing Implementation

--- Training.Angular19.Project/src/app/app.component.ts
+++ Training.Angular19.Project/src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { UserComponent } from './components/user/user.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { DataBindingComponent } from './components/data-binding/data-binding.component';
-import { NgIfComponent } from './components/ng-if/ng-if.component';
-import { NgForComponent } from './components/ng-for/ng-for.component';
-import { NgClassComponent } from './components/ng-class/ng-class.component';
-import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+// import { UserComponent } from './components/user/user.component';
+// import { AdminComponent } from './components/admin/admin.component';
+// import { DataBindingComponent } from './components/data-binding/data-binding.component';
+// import { NgIfComponent } from './components/ng-if/ng-if.component';
+// import { NgForComponent } from './components/ng-for/ng-for.component';
+// import { NgClassComponent } from './components/ng-class/ng-class.component';
+// import { NgStyleComponent } from './components/ng-style/ng-style.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,UserComponent,AdminComponent,DataBindingComponent,NgIfComponent,NgForComponent, NgClassComponent,NgStyleComponent],
+  //imports: [RouterOutlet,UserComponent,AdminComponent,DataBindingComponent,NgIfComponent,NgForComponent, NgClassComponent,NgStyleComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
