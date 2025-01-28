7bbd61d7955be31d31e6c05126df7050ee70b75c
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-29 02:29:36 +0530

    Part-6:Angular19-@if @else control flow statement implementation using checkbox

--- Training.Angular19.Project/src/app/components/control-statement/control-statement.component.ts
+++ Training.Angular19.Project/src/app/components/control-statement/control-statement.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-statement',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-statement.component.html',
   styleUrl: './control-statement.component.css'
 })
 export class ControlStatementComponent {
 div1Visible:boolean = false;
+isChecked:boolean = false;
+
 hideShowDiv1(isShow:boolean)
 {
 this.div1Visible=isShow;
