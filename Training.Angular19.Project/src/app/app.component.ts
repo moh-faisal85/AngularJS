89c7dd23c0bfc4efbaec3d7ea46987bafecb1785
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-28 18:05:04 +0530

    Research:Implementing Scroll top icon

--- Training.Angular19.Project/src/app/app.component.ts
+++ Training.Angular19.Project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 // import { UserComponent } from './components/user/user.component';
 // import { AdminComponent } from './components/admin/admin.component';
@@ -11,10 +12,22 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet,UserComponent,AdminComponent,DataBindingComponent,NgIfComponent,NgForComponent, NgClassComponent,NgStyleComponent],
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Training.Angular19.Project';
+  showScroll: boolean = false;
+
+  // Detect scroll events
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScroll = window.pageYOffset > 200;
+  }
+
+  // Scroll to top logic
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
