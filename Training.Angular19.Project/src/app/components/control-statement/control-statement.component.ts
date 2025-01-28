80f79fa47c7552e2c8d41a44eb614d80ac572902
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-29 02:46:46 +0530

    Part-6:Angular19-@for control flow statement implementation using array and table

--- Training.Angular19.Project/src/app/components/control-statement/control-statement.component.ts
+++ Training.Angular19.Project/src/app/components/control-statement/control-statement.component.ts
@@ -11,6 +11,21 @@ export class ControlStatementComponent {
 div1Visible:boolean = false;
 isChecked:boolean = false;
 dayName:string='';
+cityList:string []= [
+  "Pune",
+  "Chennai",
+  "Mumbai",
+  "Bengalure"
+];
+
+employeeArray : any = [
+  {empId:1,name:'Faisal',city :'Hyderabad',contactNo:'9999999999'},
+  {empId:2,name:'Karthik',city :'Bengalure',contactNo:'8888888888'},
+  {empId:3,name:'Sambath',city :'Chennai',contactNo:'7777777777'},
+  {empId:4,name:'Mano',city :'Pune',contactNo:'6666666666'},
+  {empId:5,name:'Rajesh',city :'Mumbau',contactNo:'5555555555'}
+]
+
 hideShowDiv1(isShow:boolean)
 {
 this.div1Visible=isShow;
