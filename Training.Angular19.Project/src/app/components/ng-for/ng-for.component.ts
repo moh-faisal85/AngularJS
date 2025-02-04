51f57a9cbfe386d7d06dd5df1d8d41b06b0d9059
moh-faisal85 <moh.faisal85@gmail.com>
2025-02-05 01:09:37 +0530

    Part-20-Angular19-@Input feature dynamic OBJECT value progress bar implementation

--- Training.Angular19.Project/src/app/components/ng-for/ng-for.component.ts
+++ Training.Angular19.Project/src/app/components/ng-for/ng-for.component.ts
@@ -1,10 +1,11 @@
 import { CurrencyPipe, DatePipe, JsonPipe, LowerCasePipe, NgFor, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { CustomPipePipe } from '../../pipes/custom-pipe.pipe';
+import { ProgressBarComponent } from "../../reusable/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-ng-for',
-  imports: [NgFor, UpperCasePipe, LowerCasePipe, JsonPipe, DatePipe, CurrencyPipe, TitleCasePipe],
+  imports: [NgFor, UpperCasePipe, LowerCasePipe, JsonPipe, DatePipe, CurrencyPipe, TitleCasePipe, ProgressBarComponent],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
@@ -14,10 +15,10 @@ export class NgForComponent {
   cityList:string[] = ["Pune", "Chennai", "Mumbai", "Tirunelveli"];
   currentDate : Date = new Date();
   employeeArray : any = [
-    {empId:1,name:'Faisal',city :'Hyderabad',contactNo:'9999999999',salary:1500},
-    {empId:2,name:'Karthik',city :'Bengalure',contactNo:'8888888888',salary:1000},
-    {empId:3,name:'Sambath',city :'Chennai',contactNo:'7777777777',salary:2000},
-    {empId:4,name:'Mano',city :'Pune',contactNo:'6666666666',salary:5000},
-    {empId:5,name:'Rajesh',city :'Mumbau',contactNo:'5555555555',salary:10000}
+    {empId:1,name:'Faisal',city :'Hyderabad',contactNo:'9999999999',salary:1500, attendance:100},
+    {empId:2,name:'Karthik',city :'Bengalure',contactNo:'8888888888',salary:1000, attendance:50},
+    {empId:3,name:'Sambath',city :'Chennai',contactNo:'7777777777',salary:2000, attendance:80},
+    {empId:4,name:'Mano',city :'Pune',contactNo:'6666666666',salary:5000, attendance:30},
+    {empId:5,name:'Rajesh',city :'Mumbau',contactNo:'5555555555',salary:10000, attendance:40}
   ]
 }
