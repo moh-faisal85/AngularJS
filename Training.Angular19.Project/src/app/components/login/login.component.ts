eada1fdebb148c777d1d57b22e032a8a4122901e
moh-faisal85 <moh.faisal85@gmail.com>
2025-02-07 01:51:28 +0530

    Part-22-Angular19-Following changes incorporated 1.Hard Coded Login Logic implemented

--- Training.Angular19.Project/src/app/components/login/login.component.ts
+++ Training.Angular19.Project/src/app/components/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj:any = {
+    username :  '',
+    password :  ''
+  };
+  router = inject(Router);
 
+  onLogin(){
+    if(this.loginObj.username == "admin" && this.loginObj.password == "1122"){
+      this.router.navigateByUrl("admin");
+    }
+    else
+    {
+      alert("Wrong Credentials")
+    }
+  }
 }
