b51ed097eed75fe404071abf06dba7afe47f2b07
moh-faisal85 <moh.faisal85@gmail.com>
2025-02-15 17:26:23 +0530

    Part-22-Angular19-Static and Api Login Logic got implemented

--- Training.Angular19.Project/src/app/components/layout/layout.component.ts
+++ Training.Angular19.Project/src/app/components/layout/layout.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -8,5 +8,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-
+  route = inject(Router);
+  logOut()
+  {
+    localStorage.removeItem("angular19User");
+    
+    this.route.navigateByUrl("login");
+  }
 }
--- Training.Angular19.Project/src/app/components/login/login.component.ts
+++ Training.Angular19.Project/src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -13,15 +14,32 @@ export class LoginComponent {
     username :  '',
     password :  ''
   };
+
+  apiLoginObj:any = {
+    emailId:"Test@faisal.com",
+    password:"111"
+
+
+  };
   router = inject(Router);
+  http = inject(HttpClient);
 
   onLogin(){
-    if(this.loginObj.username == "admin" && this.loginObj.password == "1122"){
+    // if(this.loginObj.username == "admin" && this.loginObj.password == "1122"){
+    //   this.router.navigateByUrl("admin");
+    // }
+    // else
+    // {
+    //   alert("Wrong Credentials")
+    // }
+    //Note: Update the API domain name in proxy.conf.json file - "target": "https://projectapi.gerasim.in",
+    this.http.post("/api/UserApp/login" , this.apiLoginObj).subscribe((res:any) => {
+      debugger;
+      localStorage.setItem("angular19User", res.data.userId);
       this.router.navigateByUrl("admin");
-    }
-    else
-    {
-      alert("Wrong Credentials")
-    }
+    }, error => {
+      debugger;
+      alert("Wrong Credentials");
+    })
   }
 }
